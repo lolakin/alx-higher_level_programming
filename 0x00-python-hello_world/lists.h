32365123856e45867b5713cc895f396203e4aaf7
Lois Lade <96300947+lolakin@users.noreply.github.com>
2022-06-27 20:51:17 +0100

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,26 +1,26 @@
-#ifndef LISTS.H
-#define LISTS.H
-
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * check_cycle - function checks if a linked list conatins a cycle
+ * @list: linked list to check
  *
- * Description: singly linked list node structure
- * for alx project
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-typedef struct listint_s
+
+int check_cycle(listint_t *list)
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	listint_t *fast = list;
+	listint_t *slow = list;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t  *head);
-int check_cycle(listint_t *list);
+	if (!list)
+		return (0);
 
-#endif /* LISTS_H * /
-	
+	while (slow && fast && fast -> next)
+	{
+		slow = slow -> next;
+		fast = fast -> next -> next;
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
+}
